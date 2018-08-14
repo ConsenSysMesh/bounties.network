a1e6970dc7f33e3a635773b2f66e95d630e5ad99
Tony Crowe <github@tonycrowe.com>
2018-08-14 17:40:03 -0600

    remove carousel test numbering

--- src/data/media.js
+++ src/data/media.js
@@ -8,72 +8,63 @@ It's used at the bottom of ./src/index.ejs
 
 let media = [
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '1',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '2',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '3',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '4',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '5',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '6',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '7',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '8',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
-    // title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    title: '9',
+    title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
     iconUrl: '/img/social/medium.png',
     imageUrl: '/img/media/medium-honu-kitty.png',
     websiteUrl:
