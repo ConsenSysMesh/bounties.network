77307ff827c5f2290209be8b7e5788045cefbfe9
mbeylin <mark_beylin@hotmail.com>
2018-09-06 12:39:33 -0400

    updates to make it work

--- src/data/media.js
+++ src/data/media.js
@@ -9,63 +9,63 @@ It's used at the bottom of ./src/index.ejs
 let media = [
   {
     title: 'ConsenSys Bounties Network Simona Pop on Building a World on Bounties',
-    iconUrl: '/img/social/coincentral.png',
-    imageUrl: '/img/media/coincentral.png',
+    iconUrl: './img/social/coincentral.png',
+    imageUrl: './img/media/coincentral.png',
     websiteUrl:
       'https://coincentral.com/bounties-network-simona-pop/'
   },
   {
     title: 'Forget Prices, Ethereum Is Offering a Different Value in Afghanistan',
-    iconUrl: '/img/social/coindesk.png',
+    iconUrl: './img/social/coindesk.png',
     imageUrl: 'https://media.coindesk.com/uploads/2018/06/IMG_8904-860x430.jpg',
     websiteUrl:
       'https://www.coindesk.com/afghanistan-ethereum-code-to-inspire-value/'
   },
   {
     title: 'How Moksha Coin Galvanized Over 1/3 of the Community at ETHIndia',
-    iconUrl: '/img/social/medium.png',
-    imageUrl: '/img/media/medium-moksha.jpeg',
+    iconUrl: './img/social/medium.png',
+    imageUrl: './img/media/medium-moksha.jpeg',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
     title: '“There’s a dApp for That” Top 40 Decentralized Apps You Can Use Right Now',
-    iconUrl: '/img/social/beg.png',
-    imageUrl: '/img/media/dapps.jpg',
+    iconUrl: './img/social/beg.png',
+    imageUrl: './img/media/dapps.jpg',
     websiteUrl:
       'https://bitcoinexchangeguide.com/theres-a-dapp-for-that-top-40-decentralized-apps-you-can-use-right-now/'
   },
   {
     title: 'Delphi Dev Diary 0: The Delphi Sessions',
-    iconUrl: '/img/social/medium.png',
-    imageUrl: '/img/media/devdiaries.jpeg',
+    iconUrl: './img/social/medium.png',
+    imageUrl: './img/media/devdiaries.jpeg',
     websiteUrl:
       'https://medium.com/bounties-network/delphi-dev-diary-0-the-delphi-sessions-785e276a51ad'
   },
   {
     title: 'Crypto Bounty Hunting Is Becoming a High-Tech Way Out of Poverty',
-    iconUrl: '/img/social/coindesk.png',
+    iconUrl: './img/social/coindesk.png',
     imageUrl: 'https://media.coindesk.com/uploads/2018/07/Screen-Shot-2018-07-08-at-9.41.14-PM-860x430.png',
     websiteUrl:
       'https://www.coindesk.com/crypto-bounty-hunting-is-offering-a-way-out-of-poverty/'
   },
   {
     title: 'Warum es einen Berlin Coin braucht – Ein Interview mit Simona Pop vom...',
-    iconUrl: '/img/social/btc-echo.png',
-    imageUrl: '/img/media/simona.jpg',
+    iconUrl: './img/social/btc-echo.png',
+    imageUrl: './img/media/simona.jpg',
     websiteUrl:
       'https://www.btc-echo.de/warum-es-einen-berlin-coin-braucht-ein-interview-mit-simona-pop-vom-bounties-network/0'
   },
   {
     title: 'The Honu Kitty: Supporting Ocean Conservation the Crypto Way',
-    iconUrl: '/img/social/medium.png',
-    imageUrl: '/img/media/medium-honu-kitty.png',
+    iconUrl: './img/social/medium.png',
+    imageUrl: './img/media/medium-honu-kitty.png',
     websiteUrl:
       'https://medium.com/bounties-network/the-honu-kitty-supporting-ocean-conservation-the-crypto-way-ef1042e58c50'
   },
   {
     title: 'Meet the Dapp Market: A Twist On Open Source Is Winning Developers',
-    iconUrl: '/img/social/coindesk.png',
+    iconUrl: './img/social/coindesk.png',
     imageUrl: 'https://media.coindesk.com/uploads/2018/06/computers-pcs-860x430.jpg',
     websiteUrl:
       'https://www.coindesk.com/meet-dapp-market-twist-open-source-winning-developers/'
--- src/data/partners.js
+++ src/data/partners.js
@@ -9,57 +9,57 @@ See ./src/index.ejs
 let partners = [
   {
     title: 'CONSENSYS',
-    imageUrl: '/img/partnerships/consensys.png',
+    imageUrl: './img/partnerships/consensys.png',
     websiteUrl: 'https://new.consensys.net'
   },
   {
     title: 'GITCOIN',
-    imageUrl: '/img/partnerships/gitcoin.png',
+    imageUrl: './img/partnerships/gitcoin.png',
     websiteUrl: 'https://gitcoin.co'
   },
   {
     title: 'ETHBERLIN',
-    imageUrl: '/img/partnerships/ethberlin.png',
+    imageUrl: './img/partnerships/ethberlin.png',
     websiteUrl: 'https://ethberlin.com'
   },
   {
     title: 'MAKER DAO',
-    imageUrl: '/img/partnerships/makerDao.svg',
+    imageUrl: './img/partnerships/makerDao.svg',
     websiteUrl: 'https://www.makerdao.com'
   },
   {
     title: 'STATUS',
-    imageUrl: '/img/partnerships/status.svg',
+    imageUrl: './img/partnerships/status.svg',
     websiteUrl: 'https://status.im'
   },
   {
     title: 'CODE TO INSPIRE',
-    imageUrl: '/img/partnerships/codeToInspire.png',
+    imageUrl: './img/partnerships/codeToInspire.png',
     websiteUrl: 'http://codetoinspire.org'
   },
   {
     title: 'CELLARIUS',
-    imageUrl: '/img/partnerships/cellarius.png',
+    imageUrl: './img/partnerships/cellarius.png',
     websiteUrl: 'https://cellarius.network'
   },
   {
     title: 'ETHINDIA',
-    imageUrl: '/img/partnerships/ethindia.svg',
+    imageUrl: './img/partnerships/ethindia.svg',
     websiteUrl: 'https://ethindia.co'
   },
   {
     title: 'KAURI',
-    imageUrl: '/img/partnerships/kauri.png',
+    imageUrl: './img/partnerships/kauri.png',
     websiteUrl: 'https://kauri.io'
   },
   {
     title: 'BSIC',
-    imageUrl: '/img/partnerships/BSIC.png',
+    imageUrl: './img/partnerships/BSIC.png',
     websiteUrl: 'https://www.blockchainforsocialimpact.com'
   },
   {
     title: 'ACTAI GLOBAL',
-    imageUrl: '/img/partnerships/actai.png',
+    imageUrl: './img/partnerships/actai.png',
     websiteUrl: 'http://actai.global'
   },
 ]
