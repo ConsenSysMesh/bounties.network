1e5923796721409348d9d4f7768aa21a86efab4f
mbeylin <mark_beylin@hotmail.com>
2018-09-05 23:18:04 -0400

    added small copy changes, added media and partners, added links + SEO

--- src/data/partners.js
+++ src/data/partners.js
@@ -18,9 +18,24 @@ let partners = [
     websiteUrl: 'https://gitcoin.co'
   },
   {
-    title: 'status',
+    title: 'ETHBERLIN',
+    imageUrl: '/img/partnerships/ethberlin.png',
+    websiteUrl: 'https://ethberlin.com'
+  },
+  {
+    title: 'MAKER DAO',
+    imageUrl: '/img/partnerships/makerDao.svg',
+    websiteUrl: 'https://www.makerdao.com'
+  },
+  {
+    title: 'STATUS',
     imageUrl: '/img/partnerships/status.svg',
     websiteUrl: 'https://status.im'
+  },
+  {
+    title: 'CODE TO INSPIRE',
+    imageUrl: '/img/partnerships/codeToInspire.png',
+    websiteUrl: 'http://codetoinspire.org'
   },
   {
     title: 'CELLARIUS',
@@ -28,25 +43,25 @@ let partners = [
     websiteUrl: 'https://cellarius.network'
   },
   {
-    title: 'CONSENSYS',
-    imageUrl: '/img/partnerships/consensys.png',
-    websiteUrl: 'https://new.consensys.net'
+    title: 'ETHINDIA',
+    imageUrl: '/img/partnerships/ethindia.svg',
+    websiteUrl: 'https://ethindia.co'
   },
   {
-    title: 'GITCOIN',
-    imageUrl: '/img/partnerships/gitcoin.png',
-    websiteUrl: 'https://gitcoin.co'
+    title: 'KAURI',
+    imageUrl: '/img/partnerships/kauri.png',
+    websiteUrl: 'https://kauri.io'
   },
   {
-    title: 'status',
-    imageUrl: '/img/partnerships/status.svg',
-    websiteUrl: 'https://status.im'
+    title: 'BSIC',
+    imageUrl: '/img/partnerships/BSIC.png',
+    websiteUrl: 'https://www.blockchainforsocialimpact.com'
   },
   {
-    title: 'CELLARIUS',
-    imageUrl: '/img/partnerships/cellarius.png',
-    websiteUrl: 'https://cellarius.network'
-  }
+    title: 'ACTAI GLOBAL',
+    imageUrl: '/img/partnerships/actai.png',
+    websiteUrl: 'http://actai.global'
+  },
 ]
 
 module.exports = partners
